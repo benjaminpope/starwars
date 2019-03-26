8025f50f7d6a05f130afa33269d2d61fb98f94a0
grd349 <grd349@gmail.com>
2019-03-26 11:13:00 +0000

    GAN working on a simple 2 period sin wave

--- gan/gan.py
+++ gan/gan.py
@@ -39,24 +39,26 @@ class gan():
         self.d_model.add(Dense(self.gen_shape['d1'],
                            input_dim=self.lc_size))
         self.d_model.add(LeakyReLU(alpha=0.2))
+        self.d_model.add(Dropout(0.3))
         self.d_model.add(Dense(self.gen_shape['d2']))
         self.d_model.add(LeakyReLU(alpha=0.2))
+        self.d_model.add(Dropout(0.3))
         self.d_model.add(Dense(1, activation='sigmoid'))
         print(self.d_model.summary())
 
     def model(self):
-        z = Input(shape=(self.lc_size, ))
-        img = self.gen_model(z)
-        self.d_model.trainable = False
-        real = self.d_model(img)
-        self.combined = Model(z, real)
-        gen_optimizer = Adam(lr=0.0001, beta_1=0.1)
-        disc_optimizer = Adam(lr=0.0001, beta_1=0.1)
+        gen_optimizer = Adam(lr=0.0001, beta_1=0.5)
+        disc_optimizer = Adam(lr=0.0001, beta_1=0.5)
         self.d_model.compile(loss='binary_crossentropy',
                              optimizer=disc_optimizer,
                              metrics=['accuracy'])
         self.gen_model.compile(loss='binary_crossentropy',
                                optimizer=gen_optimizer)
+        self.d_model.trainable=False
+        z = Input(shape=(self.lc_size, ))
+        img = self.gen_model(z)
+        real = self.d_model(img)
+        self.combined = Model(inputs=z, outputs=real)
         self.combined.compile(loss='binary_crossentropy',
                               optimizer=gen_optimizer)
 
@@ -67,7 +69,7 @@ class gan():
         for epoch in range(epochs + 1):
             for batch in range(num_batches):
                 # Noise images
-                noise = np.random.normal(0, 1, (half_batch, self.lc_size))
+                noise = np.random.uniform(-1, 1, (half_batch, self.lc_size))
                 fake_images = self.gen_model.predict(noise)
                 fake_labels = np.zeros((half_batch, 1)) # label is 0
                 # Real images ...
@@ -75,24 +77,31 @@ class gan():
                 real_images = self.X_train[idx]
                 real_labels = np.ones((half_batch, 1)) # label is 1
                 # Train the discriminator (real classified as ones and generated as zeros)
+                self.d_model.trainable=True
                 d_loss_real = self.d_model.train_on_batch(real_images, real_labels)
                 d_loss_fake = self.d_model.train_on_batch(fake_images, fake_labels)
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-                noise = np.random.normal(0, 1, (batch_size, self.lc_size))
+                self.d_model.trainable=False
+                noise = np.random.uniform(-1, 1, (batch_size, self.lc_size))
                 # Train the generator
                 g_loss = self.combined.train_on_batch(noise, np.ones((batch_size, 1)))
                 # Plot the progress
                 print(f"Epoch: {epoch} D loss: {d_loss[0]} G loss: {g_loss}")
+                if epoch % 10 == 0:
+                    if batch % 50 == 0:
+                        self.save_imgs(epoch, batch)
 
     def get_data(self, nd=1000):
-        x = np.linspace(0, 6.0*np.pi, self.lc_size)
+        x = np.linspace(0, 4*np.pi, self.lc_size)
         self.X_train = np.zeros([nd, self.lc_size])
         for i in range(nd):
-            self.X_train[i, :] = np.sin(x)# + np.random.rand()*np.pi*2)
+            self.X_train[i, :] = np.sin(x) + np.random.randn(self.lc_size)*0.01
+        print(f'Data shape : {self.X_train.shape}')
 
     def make_img(self):
         noise = np.random.normal(0, 1, (10, self.lc_size))
         fake_images = self.gen_model.predict(noise)
+        print(f'Fake images shape : {fake_images.shape}')
         fig, ax = plt.subplots()
         ax.plot(fake_images.T)
 
@@ -101,6 +110,21 @@ class gan():
         fig, ax = plt.subplots()
         ax.plot(self.X_train.T)
 
+    def save_imgs(self, epoch, batch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r*c, self.lc_size))
+        gen_imgs = self.gen_model.predict(noise)
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, ax = plt.subplots()
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                ax.plot(gen_imgs[cnt, :])
+                cnt += 1
+        fig.savefig("images/lc_%d_%d.png" % (epoch, batch))
+        plt.close()
+
 if __name__ == "__main__":
     starwars = gan()
     starwars.make_generator()
@@ -108,10 +132,8 @@ if __name__ == "__main__":
     starwars.model()
     starwars.plot_some_data()
     plt.show()
-    starwars.get_data()
-    for i in range(10):
-        starwars.train()
-        starwars.make_img()
-        plt.show()
+    starwars.get_data(nd=10000)
+    starwars.make_img()
+    starwars.train(epochs=200)
     starwars.plot_some_data()
     plt.show()
